94b548ea64c5eefde009c471ce686aee9676b331
hamza192 <41551916+hamza192@users.noreply.github.com>
2018-08-27 17:15:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -214,6 +214,7 @@ message.channel.sendEmbed(cat);
 
 
 client.on("message", async message => {
+var prefix = "-";
 var aoasm =[
     {q:"ما عاصمة **المغرب**",a:"الرباط"},
     {q:"ما عاصمة **افغانستان**",a:"كبل"},
@@ -279,7 +280,7 @@ var aoasm =[
     {q:"ما عاصمة **كندا  **",a:"اوتاوا"},
     {q:"ما عاصمة **البرازيل  **",a:"برازيليا"},
    ];
-    if(message.content == "عواصم"){
+    if(message.content == prefix+"عواصم"){
         if(UserBlocked.has(message.guild.id)) return message.channel.send("هناك جلسة .")
         UserBlocked.add(message.guild.id)
         var ask = aoasm[Math.floor(Math.random() * aoasm.length)];
