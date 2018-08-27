ee95313e9708a2275e3a147382fb76599d81d2fb
hamza192 <41551916+hamza192@users.noreply.github.com>
2018-08-27 09:44:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -163,6 +163,7 @@ reaction3.on("collect", r => {
 
 
 
+let points = {};
 const type = [
     {
             "type": "https://cdn.discordapp.com/attachments/429298913980317696/429298994078810127/a90c6b270eb8bb2e.png",
@@ -462,7 +463,7 @@ if (!points[message.author.id]) points[message.author.id] = {
     points: 0,
   };
   if(!message.guild) return;
- 
+    let id = message.author.id,prefix="-";
     if (spee[id] && (new Date).getTime() - spee[id] < 15*1000) {
         let r = (new Date).getTime() - spee[id];
         r = 15*1000 - r;
